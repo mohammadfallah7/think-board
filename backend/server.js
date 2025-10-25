09baf47a36bf23a1663e61d3aa280ecc35fb65f1
Mohammad_Fallah <mohammadfallah.w@gmail.com>
2025-10-25 14:51:38 +0330

    feat: :sparkles: Create routes

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,17 @@ import express from "express";
 
 const app = express();
 
-app.get("/api", (req, res) => {
-  res.send("Hello from the backend");
+app.get("/api/notes", (req, res) => {
+  res.status(200).json({ message: "You got the notes successfully!" });
+});
+app.post("/api/notes", (req, res) => {
+  res.status(201).json({ message: "You created a note successfully!" });
+});
+app.put("/api/notes/:id", (req, res) => {
+  res.status(200).json({ message: "You updated a note successfully!" });
+});
+app.delete("/api/notes/:id", (req, res) => {
+  res.status(200).json({ message: "You deleted a note successfully!" });
 });
 
 app.listen(5001, () => {
