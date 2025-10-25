1136d6e9882a58b19687c6963eb8a06e68d73aa4
Mohammad_Fallah <mohammadfallah.w@gmail.com>
2025-10-25 14:11:06 +0330

    feat: :sparkles: Create first API

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/api", (req, res) => {
+  res.send("Hello from the backend!");
+});
+
 app.listen(5001, () => {
   console.log("Server started on port: 5001");
 });
