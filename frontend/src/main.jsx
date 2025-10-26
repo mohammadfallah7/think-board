7ed719918fdcbece42a3c68fab9dce26f9422015
Mohammad_Fallah <mohammadfallah.w@gmail.com>
2025-10-26 16:12:07 +0330

    feat: :sparkles: Add toaster to the application

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Toaster } from "react-hot-toast";
 import { RouterProvider } from "react-router";
 import "./index.css";
 import router from "./router";
@@ -7,5 +8,6 @@ import router from "./router";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
+    <Toaster />
   </StrictMode>
 );
