707fcf42e343389e83c36e347f9488de583b84f4
Mohammad_Fallah <mohammadfallah.w@gmail.com>
2025-10-28 17:50:55 +0330

    refactor: :recycle: Refactor create note page using custom hook

--- frontend/src/pages/CreateNotePage.jsx
+++ frontend/src/pages/CreateNotePage.jsx
@@ -1,45 +1,13 @@
 import { LucideArrowLeft, LucideLoader2 } from "lucide-react";
-import { useState } from "react";
-import { toast } from "react-hot-toast";
-import { Link, useNavigate } from "react-router";
-import { axiosInstance } from "../lib/utils";
+import { Link } from "react-router";
+import useCreateNote from "../hooks/use-create-note";
 
 const CreateNotePage = () => {
-  const [note, setNote] = useState({ title: "", content: "" });
-  const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
+  const { loading, note, setNote, mutate: createNote } = useCreateNote();
 
-  const handleSubmit = async (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-
-    if (!note.title.trim() || !note.content.trim()) {
-      toast.error("All fields are required!");
-      return;
-    }
-
-    setLoading(true);
-    try {
-      await axiosInstance.post("/notes", note);
-      toast.success("Note created successfully");
-      navigate("/");
-    } catch (error) {
-      switch (error.response.status) {
-        case 400:
-          toast.error(error.response.data.message);
-          break;
-        case 429:
-          toast.error("Rate limit exceeded! Please try again later.");
-          break;
-        case 500:
-          toast.error(error.response.data.message);
-          break;
-        default:
-          toast.error("Failed to create note!");
-          break;
-      }
-    } finally {
-      setLoading(false);
-    }
+    createNote();
   };
 
   return (
