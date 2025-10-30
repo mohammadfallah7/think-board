6902fa0823eb67eef4e48761d8ab89975e55f791
Mohammad_Fallah <mohammadfallah.w@gmail.com>
2025-10-30 13:58:04 +0330

    feat: :sparkles: Serve dist folder as static file in production

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,31 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import path from "path";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middlewares/rate-limiter.js";
 import noteRouter from "./routes/note-router.js";
 
-const app = express();
-
+const __dirname = path.resolve();
 dotenv.config();
 
+const app = express();
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
 app.use(rateLimiter);
 
 app.use("/api/notes", noteRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 connectDB().then(() => {
   const port = process.env.PORT || "5001";
